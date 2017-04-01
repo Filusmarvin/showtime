afecd4351a7a37040d70c526528086c453b1b3ff
Marvin Filus <marvinfi@Marvins-Air.local.tld>
2017-04-01 19:19:53 -0400

    level 1

--- index.js
+++ index.js
@@ -8,84 +8,101 @@
 //
 // how many movies have the stars of the most popular movie of last year appeared in? (list each star's name with the number of movies)
 
-
+var imgUrl = 'https://image.tmdb.org/t/p/'
 
 function getData (url, params) {
      let key = "81599007ff214265c13a0888da791d0c";
-     return fetch(`https://api.themoviedb.org/3/${url}?api_key=${key}&${params}`)
-       .then(response => response.json())
-
+     return fetch(`https://api.themoviedb.org/3/${url}?api_key=${key}&query=${params}`)
+     .then(response => response.json())
    }
-   getData('movie/popular').then(json => console.log(json));
 
-   function getMovie (movieData) {
-     console.log('working with ', movieData.title);
-   }
-   getData('movie/popular').then(json => json.results.map(getMovie));
+// Hard Coded into the page.
+
+// function spiderman (movie)
+
+function spiderMan(spidey){
+  let spideyTitle = spidey.title;
+  let spideyPic = spidey.poster_path;
+  //console.log();
+  let enterName = document.querySelector('.name');
+  let enterPic = document.querySelector('.pic')
+  enterName.innerText = spideyTitle;
+  extraSpideyInfo(spidey)
+  // enterPic.innerHTML = This was supposed to be the picture.
+}
+
+function extraSpideyInfo (info) {
+  let spideyOverview = document.querySelector('.overview')
+  let spideyPopularity = document.querySelector('.popularity')
+  let spideyVotes = document.querySelector('.votes')
+  let spideyRelease = document.querySelector('.released')
+   spideyOverview.innerText = info.overview
+   spideyPopularity.innerText = info.popularity
+   spideyVotes.innerText = info.vote_average
+   spideyRelease.innerText = info.release_date
+}
+
+let textInput = document.getElementById('text-field').value;
+console.log(textInput)
+getData('search/movie',' spider man').then(json => json.results[0]).then(spiderMan);
+
+function handleFormSubmit(event) {
+  if(event) {
+    event.preventDefault()
+  }
+  let textInput = document.getElementById('text-field').value;
+  console.log(textInput)
+  return false;
+}
+
+var form = document.getElementById('the-form');
+if (form.attachEvent) {
+    form.attachEvent("submit", handleFormSubmit);
+} else {
+    form.addEventListener("submit", handleFormSubmit);
+}
+console.log(textInput)
+
+// Question Number Two. which genre has the highest average popularity for its top 25 movies?
+// I know how to use the ID's.
+//
+
+// function discover(movies) {
+//   console.log('This is the movie names ' , movies.title)
+// }
+//
+//
+// getData('discover/movie').then( json => json.results.map(discover));
+//
+//   // Genres Names and the Genres ID's
+//
+//    function getMovie (movieData) {
+//      console.log('The Genres Name' , movieData.name)
+//      console.log('The Genres ID' , movieData.id);
+//       // Horror Id 27
+//     }
+
+
+  //  getData('genre/movie/list').then(json => json.genres.map(getMovie)).then();
+
+
+  //  Question Number Three. which of the top 25 horror movies have no spoken language besides English?
+  //  I've gotten the top 25 movies but I do not have all the spoken languages unless I go to each and everyone of them and get them.
+  // Do not know if that is what he wants us to do to get that answer.
+
+  // function horrorMovies (movies){
+  //   console.log('The Horror Movie' , movies.title);
+  // }
+  //
+  // getData( 'genre/27/movies').then(json => json.results.map(horrorMovies));
 
 
 
+  // Question Number 4.  Who had the most starring roles in the top 25 comedies?
 
 
-   function getData (url, params) {
-     let key = "1922c66ebaed294ac65a15f52834a49b";
-     return fetch(`https://api.themoviedb.org/3/${url}?api_key=${key}&${params}`)
-       .then(response => response.json())
-
-   }
-   function getMovie (movieData) {
-     return getData(`movie/${movieData.id}`)
-   }
-   function combineBudget (total, movie) {
-     console.log('current movie budget', movie.budget);
-     return total + movie.budget;
-   }
-   function showCombinedBudget (movies) {
-     let allTheMonies = movies.reduce(combineBudget, 0);
-     console.log('these things cost ', allTheMonies);
-   }
-   getData('movie/popular')
-     .then(json => {
-       let topTen = json.results.slice(0, 10);
-       return topTen.map(getMovie)
-     })
-     .then(moviePromises => Promise.all(moviePromises))
-     .then(showCombinedBudget);
-
-
-     //
-
-     <script>
-   function getData (url, params) {
-     let key = "1922c66ebaed294ac65a15f52834a49b";
-     return fetch(`https://api.themoviedb.org/3/${url}?api_key=${key}&${params}`)
-       .then(response => response.json())
-
-   }
-   function getMovie (movieData) {
-     return getData(`movie/${movieData.id}`)
-   }
-   function combineBudget (total, movie) {
-     console.log('current movie budget', movie.budget);
-     return total + movie.budget;
-   }
-   function showCombinedBudget (movies) {
-     let allTheMonies = movies.reduce(combineBudget, 0);
-     console.log('these things cost ', allTheMonies);
-   }
-   getData('movie/popular')
-     .then(json => {
-       let topTen = json.results.slice(0, 10);
-       return topTen.map(getMovie)
-     })
-     .then(moviePromises => Promise.all(moviePromises))
-     .then(showCombinedBudget);
-
-   function getActorCredits (actor) {
-     console.log('working with ', actor.name);
-     return getData(`person/${actor.id}/movie_credits`)
-   }
-   getData('discover/movie', 'primary_release_year=2016')
-     .then(json => getData(`movie/${json.results[0].id}/credits`))
-     .then(json => json.cast.map(getActorCredits))
-     .then(wtf => console.log(wtf));
+  // function comedies (movies) {
+  //   Console.log('The ')
+  // }
+  //
+  // getData('genre/movie/list').then(json => json.map(comedies))
